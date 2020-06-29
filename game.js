e7db7f7bd39f26ad987d374963de39eeb97a6b9e
anthony-kyle-mcgrath <akm@anthonykyle.co.nz>
2020-06-29 12:15:39 +1200

    Add functions: makeBlue and Hide. Fix syntax. Comment

--- game.js
+++ game.js
@@ -11,19 +11,32 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
+
+// Set color to green on right click
 function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+// Set color to blue on left click
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue');
+  updateCounts();
+}
 // CREATE FUNCTION hide HERE
+// Hide element on double click
+function hide (evt) {
+  evt.target.classList.toggle('invisible');
+  updateCounts();
+}
 
 function updateCounts () {
   var totals = {
@@ -35,11 +48,11 @@ function updateCounts () {
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
 function displayTotals (totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
   }
 }
